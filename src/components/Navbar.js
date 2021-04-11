d7cf7a0ee596688e95b04a117dea94a56ae433b3
jamfurell <hong.j92@gmail.com>
2021-04-10 20:50:08 -0700

    install materialui icon. testing navbar materialui style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { AppBar, Toolbar, makeStyles, List, Container } from "@material-ui/core"
 
+import { IconButton } from "@material-ui/core"
+import { Home } from "@material-ui/icons"
+
+const useStyles = makeStyles({
+    navbarDisplayFlex: {
+        display: `flex`,
+        justifyContent: `flexstart`
+    },
+    listDisplayFlex: {
+        display: `flex`,
+        // justifyContent: `space-between`
+        width: '100%',
+        zIndex: '1100',
+        flexshrink:'0',
+        flexdirection:'column',
+    },
+    linkText: {
+        textDecoration: `none`,
+        color: `white`,
+        top:'0',
+        left:'auto',
+        right:'0',
+        margin:'25px',
+        position:'fixed',
+    },
+    navColor:{
+        backgroundColor:'#191a1ded',
+    }
+});
+    
 function Navbar(){
+    const classes = useStyles();
+
     return (
-        <nav>
-            <a class="hamburger" href="#"></a>
-            <ul>
-                <li><Link to={'/'}>About</Link></li>
-            </ul>
-	    </nav>
+        <AppBar className={classes.navColor} position="static">
+            <Toolbar>
+                <Container maxWidth="md" className={classes.navbarDisplayFlex}>
+                    <IconButton edge="start" color="inherit" aria-label="home">
+                        <Home fontSize="large" />
+                    </IconButton>
+                    <List component="nav" aria-labelledby="main navigation" className={classes.listDisplayFlex}>
+                        <Link className={classes.linkText} to={'/'}>About</Link>
+                    </List>
+
+                </Container>
+            </Toolbar>
+        </AppBar>
+        // <nav>
+        //     <a class="hamburger" href="#"></a>
+        //     <ul>
+        //         <li><Link to={'/'}>About</Link></li>
+        //     </ul>
+	    // </nav>
     )
 }
 
