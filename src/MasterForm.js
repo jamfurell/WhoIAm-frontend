2801423d371636a8038e2e59880e09bf7cf3dc35
jamfurell <hong.j92@gmail.com>
2021-04-11 18:49:29 -0700

    add style to masterform, question.js. clean up code

--- src/MasterForm.js
+++ src/MasterForm.js
@@ -1,5 +1,8 @@
 import React, {Component, createRef} from 'react'
-import QuestionModel from './models/question'
+
+// import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
+
 import TestTakenModel from './models/TestTaken'
 import Part1 from './pages/Part1'
 import Part2 from './pages/Part2'
@@ -7,10 +10,9 @@ import Part3 from './pages/Part3'
 import Part4 from './pages/Part4'
 import Part5 from './pages/Part5'
 
-import Test1 from './pages/test/Test1'
-import Test2 from './pages/test/Test2'
 
 class MasterForm extends Component {
+
     state = {
         currentStep: 0, // Default is Step 1
         questions:[],
@@ -19,48 +21,36 @@ class MasterForm extends Component {
         conscientiousness: 0,
         emotional_stability: 0,
         intellect: 0, 
-        // extraversion2: 0,
-        // agreeableness2: 0,
-        // conscientiousness2: 0,
-        // emotional_stability2: 0,
-        // intellect2: 0, 
     }
-    choiceRef = createRef()
+
+    // create function for counting steps
     _next() {
         let currentStep = this.state.currentStep
-        // If the current step is 1 or 2, then add one on "next" button click
         currentStep = currentStep >= 5? 4: currentStep + 1
         this.setState({
             currentStep: currentStep
         })
     }
-
+    //rendering next button based on current step
     get nextButton(){
         let currentStep = this.state.currentStep;
         this._next = this._next.bind(this)
-        console.log(this.state.currentStep, "<====the currentStep")
-        // If the current step is not 3, then render the "next" button
         if(currentStep === 5){
             return(
-                <button
-                className="btn-blue"
-                type="submit">
-                    Submit
+                <button className="btn" type="submit">
+                    <span>Submit</span>
                 </button>
             )
         } else{
             return (
-                <button 
-                className="btn-blue" 
-                type="button" 
-                onClick={this._next}>
-                Next
-            </button> 
+                <button className="btn" type="button" onClick={this._next}>
+                    <span>Next</span>
+                </button> 
             )
         }
-    
     }
 
+    //storing user input value into correct state
     handleResponse = (event) =>{
         console.log(event.target.name, event.target.value)
         if (event.target.name === "extraversion"){
@@ -79,12 +69,8 @@ class MasterForm extends Component {
             let temp= parseInt(event.target.value) + this.state.intellect
             this.setState({ intellect: temp})
         }
-        // console.log(this.state.extraversion, "<======extraversion")
-        // console.log(this.state.agreeableness, "<======agreeableness")
-        // console.log(this.state.conscientiousness, "<======conscientiousness")
-        // console.log(this.state.emotional_stability, "<======emotional_stability")
-        // console.log(this.state.intellect, "<======intellect")
     }
+    //reset state when user changes between radio btn - prevent adding values
     changeResponse = (event,previous) =>{
         if (event.target.name === "extraversion"){
             let temp= parseInt(event.target.value) + this.state.extraversion - previous
@@ -104,7 +90,6 @@ class MasterForm extends Component {
         }
     }
     
-    
     handleSubmit = (event) => {
         event.preventDefault();
         const {extraversion,
@@ -112,64 +97,63 @@ class MasterForm extends Component {
             conscientiousness,
             emotional_stability,
             intellect} = this.state
-        
-        console.log("In the handleSubmit!!!")
-        console.log(extraversion, agreeableness, conscientiousness, emotional_stability, intellect )
-
         TestTakenModel.create({ extraversion, agreeableness, conscientiousness, emotional_stability, intellect })
             .then(data => {
-                console.log(data,"<=====DATA inside onsubmit takenmodel")
                 this.props.history.push(`/TestTaken/${data.TestTaken._id}`)
             }
         )
-
     }
     
     render() { 
         return (
             <React.Fragment>
-            <h3>Please read each statement carefully and then mark the appropriate response below. Select the answer that best represents the personality characteristics you currently have not you would like to have in the future. Use the following scale to record your response</h3>
-            <p> 1= very inaccurate 2= somewhat inaccurate 3=neither/nor 4= somewhat accurate 5= very accurate</p>
-            Step {this.state.currentStep +1} / 5
-    
-            <form onSubmit={this.handleSubmit}>
-
-                <Part1 
-                currentStep={this.state.currentStep}
-                handleResponse={this.handleResponse}
-                changeResponse = {this.changeResponse}
-                choiceRef={this.choiceRef}/>
-                <Part2
-                currentStep={this.state.currentStep}
-                changeResponse = {this.changeResponse}
-                handleResponse={this.handleResponse}
-                />
-                <Part3
-                currentStep={this.state.currentStep}
-                changeResponse = {this.changeResponse}
-                handleResponse={this.handleResponse}/>
-                <Part4
-                currentStep={this.state.currentStep}
-                changeResponse = {this.changeResponse}
-                handleResponse={this.handleResponse}/>
-                <Part5
-                currentStep={this.state.currentStep}
-                changeResponse = {this.changeResponse}
-                handleResponse={this.handleResponse}/>
-        
-                {this.nextButton}
-                
-            </form>
-        </React.Fragment>
-        )
+                <Grid container className="masterForm-Container" direction="column" justify="center" alignItems="center">
+                    <div className="masterform-head"> 
+                        <span className="mfdetail">
+                            <h3>Please read each statement carefully and then mark the appropriate response below. 
+                                <br></br>
+                                Select the answer that best represents the personality characteristics you currently have, not what you would like to have. 
+                                <br></br>
+                                <br></br>
+                            </h3>
+                            <h5> Use the following scale to record your response</h5>
+                        </span>
+                        <p className="masterForm-scale"> 1= very inaccurate 2= somewhat inaccurate 3=neither/nor 4= somewhat accurate 5= very accurate</p>
+                            <br></br>
+                        <h2 className="masterForm-step">Step {this.state.currentStep +1} / 5 </h2>
+                    </div>
+                            
+                    <form onSubmit={this.handleSubmit}>
+                        <Part1 
+                            currentStep={this.state.currentStep}
+                            handleResponse={this.handleResponse}
+                            changeResponse = {this.changeResponse}
+                            />
+                        <Part2
+                            currentStep={this.state.currentStep}
+                            changeResponse = {this.changeResponse}
+                            handleResponse={this.handleResponse}
+                            />
+                        <Part3
+                            currentStep={this.state.currentStep}
+                            changeResponse = {this.changeResponse}
+                            handleResponse={this.handleResponse}
+                            />
+                        <Part4
+                            currentStep={this.state.currentStep}
+                            changeResponse = {this.changeResponse}
+                            handleResponse={this.handleResponse}
+                            />
+                        <Part5
+                            currentStep={this.state.currentStep}
+                            changeResponse = {this.changeResponse}
+                            handleResponse={this.handleResponse}
+                            />
+                        {this.nextButton}       
+                    </form>
+                </Grid>
+            </React.Fragment>
+            )
+        }
     }
-}
 export default MasterForm
-
-// alert(`Your test results: \n 
-// extraversion: ${extraversion} \n 
-// agreable: ${agreeableness} \n
-// conscientiousn: ${conscientiousness} \n
-// emotional_stability: ${emotional_stability}\n
-// intellect: ${intellect}`)
-//Please read each statement carefully and then mark the appropriate response below. Select the answer that best represents the personality characteristics you currently have not you would like to have in the future. Use the following scale to record your response
